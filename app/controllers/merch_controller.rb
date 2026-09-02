0d4a8df0a6c6688d0c017819e299d5a4180d2a18
himilou <jameshardy1@gmail.com>
2026-09-02 13:01:57 -0700

    merch index and show view work

--- app/controllers/merch_controller.rb
+++ app/controllers/merch_controller.rb
@@ -2,13 +2,13 @@ class MerchController < ApplicationController
   # before_action :set_post, only: %i[ show edit update destroy ]
   # before_action :authenticate_user!, only: [ :new, :edit, :create, :update, :destroy ]
   def index
-    @Products = Product.includes(:product_image).all
+    @Products = Product.includes(:primary_image)
     # @Products = Product.all
   end
 
   def show
     prod_id = params[:id].to_i
-    @Product = Product.includes(:product_image).find_by(id: prod_id)
+    @Product = Product.includes(:product_images).find_by(id: prod_id)
 
     @hello = "Hello from the merch controller for product: " + prod_id.to_s
   end
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,6 +1,10 @@
 class Product < ApplicationRecord
-  # has_many :product_images, dependent: :destroy
-  has_one :product_image, -> { where(is_primary: true).order(:created_at) },
+  has_many :product_images, dependent: :destroy
+
+
+  # reutrns the primary image for the product, if it exists using the :primary_image tag
+  # has_one :primary_product_image, -> { where(is_primary: true) }, class_name: 'ProductImage'
+  has_one :primary_image, -> { where(is_primary: true) },
     class_name: "ProductImage",
     inverse_of: :product,
     dependent: :destroy
