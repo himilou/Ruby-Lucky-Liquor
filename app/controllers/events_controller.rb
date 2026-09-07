dda56c338bc16a4f1e45bf6019928053bb6a387e
himilou <himilou@hotmail.com>
2026-09-07 12:49:22 -0700

    Clean up events_controller.rb by removing comments
    Removed commented-out code and unnecessary lines in events_controller.rb.

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -7,10 +7,7 @@ class EventsController < ApplicationController
   def events
     logger = Rails.logger
     @event_list = []
-
     begin
-      # dir_path = Rails.root.join("app/assets/images/event_img")
-      # json_path = dir_path.join("events.json")
       data = JSON.parse(File.read(EVENTS_JSON_PATH.to_s))
 
       data.each do |event|
@@ -33,17 +30,15 @@ class EventsController < ApplicationController
     filename = params[:filename].to_s
     @image_name = File.basename(filename)
     image_file = DIR_PATH.join(filename)
-    # @image_path = "event_img/#{@image_name}"
-
+    
     unless @image_name.present? && File.file?(image_file)
       raise ActionController::RoutingError, "Not Found"
       Rails.logger.error "Error: Image file not found at #{image_file}"
     end
-
-    # json_path = Rails.root.join("app/assets/images/event_img/events.json")
+    
     event_data = JSON.parse(File.read(EVENTS_JSON_PATH.to_s)).find { |event| event["filename"] == @image_name }
-
     @event_date = event_data&.dig("date")
     @event_bandname = event_data&.dig("bandnames")
   end
 end
+
