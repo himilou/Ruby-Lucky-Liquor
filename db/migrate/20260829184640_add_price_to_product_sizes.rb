37127880e01ff0337b1cb05da798a1a6e6030ad7
himilou <jameshardy1@gmail.com>
2026-09-07 20:49:34 -0700

    clear all migrations and dockerfile changes for dev environment

--- db/migrate/20260829184640_add_price_to_product_sizes.rb
+++
@@ -1,5 +0,0 @@
-class AddPriceToProductSizes < ActiveRecord::Migration[8.0]
-  def change
-    add_column :product_sizes, :price, :float
-  end
-end
--- db/migrate/20260901000000_rename_product_image_url_to_image_filename.rb
+++
@@ -1,5 +0,0 @@
-class RenameProductImageUrlToImageFilename < ActiveRecord::Migration[8.0]
-  def change
-    rename_column :product_images, :product_image_url, :image_filename
-  end
-end
