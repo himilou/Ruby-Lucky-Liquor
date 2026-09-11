086db582d80c957ec149c2014eb895dcc53c6318
himilou <jameshardy1@gmail.com>
2026-09-11 15:58:59 -0700

    Revert "This commit removes access to the products controller by removing the link in the main menu page"
    This reverts commit 3e25aa568030a1b0da849434fc6b78a588e7d9d6.

--- config/routes.rb
+++ config/routes.rb
@@ -9,33 +9,30 @@ Rails.application.routes.draw do
   get "galleryimage", to: "pages#galleryimage", as: :gallery_image
   get "up" => "rails/health#show", as: :rails_health_check
 
-  # Events controller routes
-  get "events", to: "events#events"
-  get "events/image", to: "events#image", as: :events_image
 
-  # Hours controller routes and login /logoff
   get "hours", to: "hours#main"
   patch "hours", to: "hours#update_hours", as: :update_hours
   get "hours/main"
+  # get "hours/new"
   get "hours/create"
+  # get "sessions/destroy"
 
   # Following allows controller redirects
   get  "newlogin",  to: "hours#newlogin"
   post "newlogin",  to: "hours#createlogin"
   post "changepassword", to: "hours#changepassword"
-  delete "logout", to: "hours#destroy" 
+  delete "logout", to: "hours#destroy"
 
-  end
-
-
-  # # products routes these are currently disabled as the products feature is not currently used
-  # get "products", to: "products#index"
-  # get "products/index"
-  # get "products/show"
-  # get "products/new"
-  # post "products/create", as: :products_create
-  # get "products/edit", as: :products_edit
-  # patch "products/update", as: :products_update
-  # delete "products/destroy", as: :products_destroy
-
-
+  # products routes
+  get "products", to: "products#index"
+  get "products/index"
+  get "products/show"
+  get "products/new"
+  post "products/create", as: :products_create
+  get "products/edit", as: :products_edit
+  patch "products/update", as: :products_update
+  delete "products/destroy", as: :products_destroy
+  # Events controller routes
+  get "events", to: "events#events"
+  get "events/image", to: "events#image", as: :events_image
+end
