6d0766b186b14d89dd8c8f4b649802f683f26996
himilou <jameshardy1@gmail.com>
2026-09-24 21:29:11 -0700

    added hours, minutes, seconds to filename to prevent issue where a file uploaded on the same day ended up getting deleted after it was written

--- app/controllers/menu_controller.rb
+++ app/controllers/menu_controller.rb
@@ -65,7 +65,7 @@ class MenuController < ApplicationController
           end
         end
         # Build its new unique name i.e. menufront08-23-26.jpg
-        filename = filename + Time.now.strftime("%m-%d-%y") + ".jpg"
+        filename = filename + Time.now.strftime("%m-%d-%y-%H-%M-%S") + ".jpg"
         filepath = DIR_PATH.join(filename)
         data = File.binread(uploaded_file)
         File.open(filepath, "wb") do |file|
