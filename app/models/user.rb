5ea1f774d23236645c2e4924c7eb89a11e001b5c
himilou <jameshardy1@gmail.com>
2026-09-25 12:44:43 -0700

    comment out user model class

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,9 @@
-class User < ApplicationRecord
-  has_secure_password
 
-  validates :username, presence: true, uniqueness: true
-end
+
+# Not currently used see JsonUser
+#
+# class User < ApplicationRecord
+#   has_secure_password
+
+#   validates :username, presence: true, uniqueness: true
+# end
